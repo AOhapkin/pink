f658dbc34428c4e3d4e7c935eb9caa9bd996a12b
AOhapkin <alexeyyago@gmail.com>
2020-12-14 16:11:03 +0300

    Pixel perfect модальных окон формы

--- source/js/script.js
+++ source/js/script.js
@@ -2,6 +2,10 @@ let MenuToggle = document.querySelector(".page-header__toggle");
 let siteMenu = document.querySelector(".site-list");
 let pageHeaderTop = document.querySelector(".page-header__top");
 let pageHeaderTopWrapper = document.querySelector(".page-header__top-wrapper");
+let modalError = document.querySelector(".modal--error");
+let modalButtonError = modalError.querySelector(".modal-button");
+let modalSucces = document.querySelector(".modal--succes");
+let modalButtonSucces = modalSucces.querySelector(".modal-button");
 
 // close menu + menu toggle
 
@@ -14,4 +18,18 @@ MenuToggle.addEventListener("click", function(evt) {
   siteMenu.classList.toggle("site-list--menu-open");
   pageHeaderTop.classList.toggle("page-header__top--menu-open");
   pageHeaderTopWrapper.classList.toggle("page-header__top-wrapper--menu-open");
+});
+
+// modal error
+
+modalButtonError.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  modalError.classList.remove("modal--show");
+});
+
+// modal succes
+
+modalButtonSucces.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  modalSucces.classList.remove("modal--show");
 });
