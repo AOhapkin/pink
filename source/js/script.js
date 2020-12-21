551b9d46255d5edc9e420963faabec17343411e7
AOhapkin <alexeyyago@gmail.com>
2020-12-21 20:06:18 +0300

    Векторные изображения главной страницы собраны в спрайт

--- source/js/script.js
+++ source/js/script.js
@@ -2,16 +2,17 @@ let MenuToggle = document.querySelector(".page-header__toggle");
 let siteMenu = document.querySelector(".site-list");
 let pageHeaderTop = document.querySelector(".page-header__top");
 let pageHeaderTopWrapper = document.querySelector(".page-header__top-wrapper");
+let pageHeaderIconOpen = document.querySelector(".page-header__toggle-icon--open");
+let pageHeaderIconClose = document.querySelector(".page-header__toggle-icon--close");
 // let modalError = document.querySelector(".modal--error");
 // let modalButtonError = modalError.querySelector(".modal-button");
 // let modalSucces = document.querySelector(".modal--succes");
 // let modalButtonSucces = modalSucces.querySelector(".modal-button");
 
-console.log("start");
-
 // close menu + menu toggle
 
 siteMenu.classList.remove("site-list--menu-open");
+pageHeaderIconOpen.classList.remove("page-header__toggle-icon--hide");
 pageHeaderTop.classList.remove("page-header__top--menu-open");
 pageHeaderTopWrapper.classList.remove("page-header__top-wrapper--menu-open");
 
@@ -20,6 +21,8 @@ MenuToggle.addEventListener("click", function(evt) {
   siteMenu.classList.toggle("site-list--menu-open");
   pageHeaderTop.classList.toggle("page-header__top--menu-open");
   pageHeaderTopWrapper.classList.toggle("page-header__top-wrapper--menu-open");
+  pageHeaderIconOpen.classList.toggle("page-header__toggle-icon--hide");
+  pageHeaderIconClose.classList.toggle("page-header__toggle-icon--hide");
 });
 
 // modal error
