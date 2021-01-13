e4c77b3e86a712b308d7fda9e1ce85f258023b23
AOhapkin <alexeyyago@gmail.com>
2021-01-13 15:42:49 +0300

    gulp scripts, fix default

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
 const csso = require("postcss-csso");
 const rename = require("gulp-rename");
+const uglify = require("gulp-uglify");
 const del = require("del");
 const imagemin = require("gulp-imagemin");
 const webp = require("gulp-webp");
@@ -52,6 +53,7 @@ exports.server = server;
 
 const watcher = () => {
   gulp.watch("source/sass/**/*.scss", gulp.series("styles"));
+  gulp.watch("source/js/script.js", gulp.series(scripts));
   gulp.watch("source/*.html").on("change", sync.reload);
 }
 
@@ -87,7 +89,19 @@ const html = () => {
     .pipe(gulp.dest("build"))
 }
 
-// Imagemin
+// Scripts
+
+const scripts = () => {
+  return gulp.src("source/js/script.js")
+    .pipe(uglify())
+    .pipe(rename("script.min.js"))
+    .pipe(gulp.dest("build/js"))
+    .pipe(sync.stream());
+}
+
+exports.scripts = scripts;
+
+// Images
 
 const images = () => {
   return gulp.src("source/img/*.{jpg,png,svg}")
@@ -101,7 +115,7 @@ const images = () => {
 
 exports.images = images;
 
-// Webp
+// WebP
 
 const createWebp = () => {
   return gulp.src("source/img/**/*.{jpg,png}")
@@ -117,39 +131,42 @@ const sprite = () => {
   return gulp.src("source/img/icons/*.svg")
     .pipe(svgstore())
     .pipe(rename("sprite.svg"))
-    .pipe(gulp.dest("source/img"));
+    .pipe(gulp.dest("build/img"));
 }
 
 exports.sprite = sprite;
-
-exports.default = gulp.series(
-  clean,
-  sprite,
-  gulp.parallel(
-    styles,
-    html,
-    copy,
-    createWebp
-  ),
-  gulp.series(
-    server, watcher
-  )
-);
-
-
 
 // Build
 
 const build = gulp.series(
   clean,
-  sprite,
   gulp.parallel(
     styles,
     html,
+    scripts,
+    sprite,
     copy,
     images,
     createWebp
   )
-)
+);
 
 exports.build = build;
+
+// Default
+
+exports.default = gulp.series(
+  clean,
+  gulp.parallel(
+    styles,
+    html,
+    scripts,
+    sprite,
+    copy,
+    createWebp
+  ),
+  gulp.series(
+    server,
+    watcher
+  )
+);
